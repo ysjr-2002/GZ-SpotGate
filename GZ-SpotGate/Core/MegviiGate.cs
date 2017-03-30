da00953bd41994c952ec7a66b3674d8f470fb0d1
ysj <ysjr-2002@163.com>
2017-03-30 16:01:49 +0800

    ok

---
+++ GZ-SpotGate/Core/MegviiGate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZ_SpotGate.Core
+{
+    class MegviiGate
+    {
+        private const int PORT = 5000;
+        private const string COMMAND_OPEN1 = "on1:01";
+        private const string COMMAND_OPEN2 = "on2:01";
+        private const string COMMAND_CLOSE = "off1";
+
+        private UdpClient socket = null;
+
+        public MegviiGate()
+        {
+            socket = new UdpClient();
+        }
+
+        public void In(string gateIp)
+        {
+            var buffer = GetOpenPackage(COMMAND_OPEN1);
+            Send(gateIp, buffer);
+        }
+
+        public void Out(string gateIp)
+        {
+            var buffer = GetOpenPackage(COMMAND_OPEN2);
+            Send(gateIp, buffer);
+        }
+
+        private void Send(string gateIp, byte[] buffer)
+        {
+            var ep = new IPEndPoint(IPAddress.Parse(gateIp), PORT);
+            socket.Send(buffer, buffer.Length, ep);
+        }
+
+        private byte[] GetOpenPackage(string command)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(command);
+            return buffer;
+        }
+
+        public void Dispose()
+        {
+            socket?.Close();
+        }
+    }
+}
