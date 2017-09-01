217cf63e2d65058ef94b1d0b3a442959b8b22521
ysj <ysjr-2002@163.com>
2017-09-01 18:18:12 +0800

    ok

--- WindowsFormsApplication1/FrmMain.Designer.cs
+++ WindowsFormsApplication1/FrmMain.Designer.cs
@@ -40,25 +40,28 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(176, 23);
+            this.textBox1.Location = new System.Drawing.Point(132, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(209, 22);
+            this.textBox1.Size = new System.Drawing.Size(158, 21);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "192.168.1.60";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(176, 61);
+            this.textBox2.Location = new System.Drawing.Point(132, 46);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(617, 22);
+            this.textBox2.Size = new System.Drawing.Size(464, 21);
             this.textBox2.TabIndex = 1;
-            this.textBox2.Text = "rtsp://192.168.1.12:554/media/live/1/1";
+            this.textBox2.Text = "rtsp://192.168.1.139:554/media/live/1/1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(176, 103);
+            this.button1.Location = new System.Drawing.Point(132, 77);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 29);
+            this.button1.Size = new System.Drawing.Size(76, 22);
             this.button1.TabIndex = 2;
             this.button1.Text = "connect";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,18 +70,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 27);
+            this.label1.Location = new System.Drawing.Point(63, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 3;
             this.label1.Text = "Koala IP：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 67);
+            this.label2.Location = new System.Drawing.Point(27, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.Size = new System.Drawing.Size(101, 12);
             this.label2.TabIndex = 4;
             this.label2.Text = "完整的rtsp地址：";
             // 
@@ -86,26 +91,26 @@
             // 
             this.lblState.AutoSize = true;
             this.lblState.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lblState.Location = new System.Drawing.Point(292, 105);
+            this.lblState.Location = new System.Drawing.Point(219, 79);
+            this.lblState.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblState.Name = "lblState";
-            this.lblState.Size = new System.Drawing.Size(0, 24);
+            this.lblState.Size = new System.Drawing.Size(0, 19);
             this.lblState.TabIndex = 5;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(245, 152);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(184, 114);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(352, 353);
+            this.pictureBox1.Size = new System.Drawing.Size(264, 265);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
-            // Form1
+            // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 521);
+            this.ClientSize = new System.Drawing.Size(608, 391);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblState);
             this.Controls.Add(this.label2);
@@ -114,7 +119,8 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WebSocket Test";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
